a1b200ca4f4fadbc90ea570bf30c018cfa33901e
fahima bushra <bushra.arifeen@gmail.com>
2024-07-07 01:03:19 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import { FaBeer } from 'react-icons/fa';
+const Navbar = () => {
+    return (
+        <div>
+           <FaBeer className='text-5xl' />
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { HiChatBubbleOvalLeftEllipsis } from "react-icons/hi2";
+
+const Home = () => {
+    return (
+        <div>
+        <HiChatBubbleOvalLeftEllipsis className="text-teal-300 bg-[#FFDFB7] text-7xl mx-auto" />
+        <HiChatBubbleOvalLeftEllipsis className=" bg-teal-300 text-white text-7xl mx-auto m-6" />
+        </div>
+    );
+};
+
+export default Home;
